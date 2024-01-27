5c3949e44347a6d9fc5c71d31ac3702f50f2239a
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-27 16:33:19 -0600

    Create checkWeather function and fetch weather API

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const apiKey = '8d299ad4159f7d0f13220a4d51fdb3c1';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=standard&q=seattle';
+
+async function checkWeather() {
+    const response = await fetch(`${apiUrl}&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+}
