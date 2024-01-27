9c3d09d015ec4d1184d03ce35366cd1ec0614a2a
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-27 17:16:00 -0600

    Add if() to checkWeather() that sets weatherIcon

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const apiKey = '8d299ad4159f7d0f13220a4d51fdb3c1';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
 
-const searchBox = document.querySelector('.search input').value;
+const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('weather-icon')
 
 async function checkWeather(city) {
     const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
@@ -14,9 +15,13 @@ async function checkWeather(city) {
     document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)}°F`;
     document.querySelector('.humidity').innerHTML = `${Math.round(data.main.humidity)}%`;
     document.querySelector('.wind').innerHTML = `${Math.round(data.wind.speed)} MPH`;
+
+    if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = 'images/clouds.png';
+    }
 }
 
 searchBtn.addEventListener('click', () => {
-    checkWeather();
+    checkWeather(searchBox.value);
 })
 
