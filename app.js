97fa7fb18ac7de0871236a39c04debd38e7b0939
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-27 16:45:23 -0600

    Set city, temp, humidity, wind to respective data

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const apiKey = '8d299ad4159f7d0f13220a4d51fdb3c1';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=standard&q=seattle';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=seattle';
 
 async function checkWeather() {
     const response = await fetch(`${apiUrl}&appid=${apiKey}`);
     let data = await response.json();
 
     console.log(data);
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = data.main.temp;
+    document.querySelector('.humidity').innerHTML = data.main.humidity;
+    document.querySelector('.wind').innerHTML = data.wind.speed;
 }
+
+checkWeather();
